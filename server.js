8f7ff63604655a6ee15334574bd8d6369ba199a6
Gurpreet Singh <gurpreet.singh2@incedoinc.com>
2024-06-25 09:36:22 +0530

    added node version

--- server.js
+++ server.js
@@ -67,12 +67,12 @@ await server.start();
 //     res.send("boom! ")
 // })
 
-if(process.env.NODE_ENV == "production"){
+// if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/dist"))
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
     })
-}
+// }
 
 
 app.use(
